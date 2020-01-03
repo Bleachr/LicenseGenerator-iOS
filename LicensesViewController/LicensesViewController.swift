971333d6e9b20790ecc39d4a0bf26d6375e6fe59
Kevin A. Hoogheem <kevin@bleachr.co>
2020-01-03 14:14:31 -0600

    public over open

--- LicensesViewController/LicensesViewController.swift
+++ LicensesViewController/LicensesViewController.swift
@@ -19,9 +19,9 @@ open class LicensesViewController: UIViewController {
   /// The tableView's UITableViewDataSource.
   var dataSource: LicensesDataSource!
 
-  open var textColor = UIColor.black
-  open var bodyColor = UIColor.darkGray
-  open var cellBackgroundColor: UIColor?
+  public var textColor = UIColor.black
+  public var bodyColor = UIColor.darkGray
+  public var cellBackgroundColor: UIColor?
 
   /// Boolean tracking if the constrains have been setup.
   fileprivate var didSetupConstraints = false
